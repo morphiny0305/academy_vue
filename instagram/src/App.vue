20fa43ae880ecd062ab503a8c22ce618e5f37203
morphiny0305 <valen0305@gmail.com>
2023-03-17 16:50:42 +0900

    0317

--- instagram/src/App.vue
+++ instagram/src/App.vue
@@ -5,23 +5,34 @@
         <li>Cancel</li>
       </ul>
       <ul class="header-button-right">
-        <li>Next</li>
+        <li @click="step++">Next</li>
       </ul>
       <img src="./assets/logo.png" class="logo" />
     </div>
 
 
 
-    <Container :게시물="게시물"/>
-    <button @click="more" class="more-btn">더보기</button>
+    <Container :step="step" :이미지="이미지" :게시물="게시물"/>
+    <button v-if="step == 0" @click="more" class="more-btn">더보기</button>
 
 
     <div class="footer">
       <ul class="footer-button-plus">
-        <input type="file" id="file" class="inputfile" />
+        <!-- <input type="file" id="file" class="inputfile" /> -->
+        <input @change="upload" type="file" id="file">
         <label for="file" class="input-plus">+</label>
       </ul>
     </div>
+
+    <!-- 탭메뉴 연습 -->
+    <!-- <div v-if="step == 0">내용1</div>
+    <div v-if="step == 1">내용2</div>
+    <div v-if="step == 2">내용3</div>
+    <button @click="step = 0">버튼1</button>
+    <button @click="step = 1">버튼2</button>
+    <button @click="step = 2">버튼3</button> -->
+
+    <!-- <div style="margin-top: 300px;"></div> -->
   </section>
 </template>
 
@@ -36,6 +47,8 @@ export default {
     return {
       더보기 : 0,
       게시물 : postData,
+      step:0, // 상태 기록
+      이미지 : '', // 업로드한 이미지 빈 주소 (스트링)
     }
   },
   components: {
@@ -46,13 +59,24 @@ export default {
       /* axios.post('url',{name: "kim"}).then().catch(()=>{}) */
 
       // 1. 더보기 버튼을 누르면 서버에 있는 추가 게시물을 가져옴 2. 그리고 게시하여 보여주기
-      axios.get('https://morphiny0305.github.io/testPAGE/vue/more0.json')
+      axios.get(`https://morphiny0305.github.io/testPAGE/vue/more${this.더보기}.json`)
       .then(결과=>{
-        console.log(결과.data);
+        // console.log(결과.data);
         this.게시물.push(결과.data);
+        this.더보기++;
       }).catch(()=>{
         // 실패시 실행할 코드
+        alert('데이터를 가져오지 못했지비..');
       })
+    },
+
+    upload(e){
+      // 이미지 업로드 함수
+      let file = e.target.files;
+      console.log(file[0].type);
+      let url = URL.createObjectURL(file[0]);
+      console.log(url);
+      this.step++;
     }
   }
 }
@@ -61,4 +85,5 @@ export default {
 <style>
 @import './assets/styles.css';
 button.more-btn {margin: auto; display: block;}
+
 </style>
--- instagram/src/components/Container.vue
+++ instagram/src/components/Container.vue
@@ -1,6 +1,28 @@
 <template>
   <section>
-    <Post :게시물="게시물[i]" v-for="(a, i) in 게시물" :key="i"/>
+    <div v-if="step == 0">
+      <Post :게시물="게시물[i]" v-for="(a, i) in 게시물" :key="i"/>
+    </div>
+    <!-- 게시물 작성 > 필터 선택 -->
+    <div v-if="step == 1">
+      <!-- 업로드한 이미지 -->
+      <div class="upload-image" :style="{backgroundImage: `url(${이미지})`}"></div>
+      <div class="filters">
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+      </div>
+    </div>
+
+    <!-- 게시물 작성 > 내용 작성 -->
+    <div v-if="step == 2">
+      <div class="upload-image"></div>
+      <div class="write">
+        <textarea class="white-box">text here.</textarea>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -18,11 +40,54 @@ export default {
   },
   props: {
     게시물: Object,
+    step: Number,
+    이미지: String,
   }
   
 }
 </script>
 
 <style>
-
+.upload-image{
+width: 100%;
+height: 450px;
+background: cornflowerblue;
+background-size : cover;
+}
+.filters{
+overflow-x:scroll;
+white-space: nowrap;
+}
+.filter-1 {
+width: 100px;
+height: 100px;
+background-color: cornflowerblue;
+margin: 10px 10px 10px auto;
+padding: 8px;
+display: inline-block;
+color : white;
+background-size: cover;
+}
+.filters::-webkit-scrollbar {
+height: 5px;
+}
+.filters::-webkit-scrollbar-track {
+background: #f1f1f1; 
+}
+.filters::-webkit-scrollbar-thumb {
+background: #888; 
+border-radius: 5px;
+}
+.filters::-webkit-scrollbar-thumb:hover {
+background: #555; 
+}
+.write-box {
+border: none;
+width: 90%;
+height: 100px;
+padding: 15px;
+margin: auto;
+display: block;
+outline: none;
+} 
 </style>
